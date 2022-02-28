91a3cdfb065d2d8a907445fa092d408761721742
Your Name <you@example.com>
2022-02-28 14:52:42 +0800

    fix bug

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container mx-auto px-4 pt-16">
-        <div class="popular-movies">
+    <div class="container mx-auto px-4 pt-16" style="display:flex">
+        <div class="popular-movies w-4/5">
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">@isset($msg){{ $movies[0]['type'] }}@else Phim Mới Cập Nhật @endisset</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                 @foreach ($movies as $movie)
